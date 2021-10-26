6b1404e9473f792abacd53d8fceabf62f45fe92a
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-10-26 10:36:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let rndNum;
-function myFunction(){
-  rndNum = Math.random()
+function setup(){
+rndNum = Math.random()*100
+  rndNum = Math.floor(rndNum) + 1;
+  console.log(rndNum)
   document.getElementById("myTitle").innerHTML = rndNum;
+}//end setup
+
+function myFunction(){
+  let theirGuess = document.getElementById("theirGuess").value;
+  if(theirGuess > rndNum){
+    document.getElementById("myTitle").innerHTML = "Too High";
+  }
 }//end function
