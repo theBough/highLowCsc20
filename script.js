58854b026741a37c5dd2297a890dbc9b8e7ada20
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-10-29 10:33:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let rndNum;
+let guesses;
 function setup(){
   rndNum = Math.random()*100
   rndNum = Math.floor(rndNum) + 1;
   console.log(rndNum)
+  guesses = 0;
   
 }//end setup
 
 function myFunction(){
   let theirGuess = document.getElementById("theirGuess").value;
+  guesses += 1;
+  document.getElementById("guesses").innerHTML = guesses;
   if(theirGuess > rndNum){
     document.getElementById("feedback").innerHTML = "Too High";
   }else if(theirGuess < rndNum){
