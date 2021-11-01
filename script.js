b87b66d10125a6d3023b60cfe59443dd0d8bba33
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-11-01 10:27:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let rndNum;
 let guesses;
 let time;
+let stopTimer;
 function setup(){
   rndNum = Math.random()*100
   rndNum = Math.floor(rndNum) + 1;
   console.log(rndNum)
   guesses = 0;
   time = 60;
-  setInterval(countDown, 1000);
+  stopTimer = setInterval(countDown, 1000);
 }//end setup
 
 function myFunction(){
@@ -20,9 +21,15 @@ function myFunction(){
     document.getElementById("feedback").innerHTML = "Too Low";
   }else{
     document.getElementById("feedback").innerHTML = "You Got!!";
+    clearInterval(stopTimer);
   }
 }//end function
 function countDown(){
   time = time -1;
   document.getElementById("timer").innerHTML = "Time: " + time;
+  if(time < 1){
+    document.getElementById("myButton").style.visibility = "hidden";
+    clearInterval(stopTimer);
+      document.getElementById("feedback").innerHTML = "You Lost";
+  }
 }
